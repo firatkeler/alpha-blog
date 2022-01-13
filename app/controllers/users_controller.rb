0bb83615588907a6bde26db0a1f20ea66a6c35ec
firatkeler <firatkeler@gmail.com>
2022-01-13 21:03:39 +0300

    added session feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    # @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -26,7 +32,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account was successfully updated"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'edit'
     end
