45fafcc4f137dbaff78d72f2be2ddb73eaea6725
firatkeler <firatkeler@gmail.com>
2022-01-15 21:49:53 +0300

    final commit

---
+++ test/models/category_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "the name should be present" do
+    @category.name = " "
+
+    assert_not @category.valid?
+  end
+
+  test "the name should be unq" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+
+    assert_not @category2.valid?
+  end
+
+  test "the name should not be too long" do
+    @category.name = "a" * 26;
+    
+    assert_not @category.valid?
+  end
+
+  test "the name should not be too short" do
+    @category.name = "aa";
+
+    assert_not @category.valid?
+  end
+
+end
